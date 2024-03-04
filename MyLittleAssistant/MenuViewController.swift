d96e84435feea1ef3be67d10d00f1b17f3964d81
david <david.gmzherrera28@gmail.com>
2024-03-04 17:05:52 -0600

    se grego las imagenes en png

--- MyLittleAssistant/MenuViewController.swift
+++ MyLittleAssistant/MenuViewController.swift
@@ -12,7 +12,6 @@ class MenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         let gradientLayer = CAGradientLayer()
              gradientLayer.frame = view.bounds
              gradientLayer.colors = [
@@ -27,15 +26,5 @@ class MenuViewController: UIViewController {
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
