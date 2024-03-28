7a39f2981a27a676c749eaf97af584ac410d41e2
David Herrera <david.gmzherrera28@gmail.com>
2024-03-27 21:24:24 -0600

    se agrego  en UserView codigo para obtener la informacion del usuario

--- MyLittleAssistant/UserViewController.swift
+++ MyLittleAssistant/UserViewController.swift
@@ -9,7 +9,33 @@ import UIKit
 
 class UserViewController: UIViewController {
 
+   
+    
+    
+    @IBOutlet weak var Nombre_LBL: UILabel!
+    
+    
+    @IBOutlet weak var name_txt: UILabel!
+    
+    
+    
+    
+    @IBOutlet weak var Email_LBL: UILabel!
+    
+    
+    
+    @IBOutlet weak var email_txt: UILabel!
+    
+    
+    
+    let userData = UserData.sharedData()
+    var user: User = User(id: 0, name: "", email: "")
+    var userName = ""
+    var emailUser = ""
+    var maxLenghts = [UITextField: Int]()
     override func viewDidLoad() {
+        
+        getUser()
         super.viewDidLoad()
         let gradientLayer = CAGradientLayer()
              gradientLayer.frame = view.bounds
@@ -22,17 +48,64 @@ class UserViewController: UIViewController {
                gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
                
                view.layer.insertSublayer(gradientLayer, at: 0)
+        
+                configureLabelDesign(Nombre_LBL)
+                configureLabelDesign(Email_LBL)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureLabelDesign(_ label: UILabel) {
+             label.textColor = UIColor.white
+               label.font = UIFont.boldSystemFont(ofSize: 18)
+               label.layer.cornerRadius = 3
+               label.clipsToBounds = true
+               label.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+               label.layer.borderWidth = 1.0
+               label.layer.borderColor = UIColor.gray.cgColor
+                       
     }
-    */
+    
+    func getUser() {
+          guard let url = URL(string: "https://getuser") else {
+              print("URL inválida")
+              return
+          }
+          var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
+          request.httpMethod = "GET"
+          if let token = userData.jwt {
+              request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+              request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+          } else {
+              print("No se encontró token de autenticación")
+              return
+          }
+          let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+              if let error = error {
+                  print("Error en el request: \(error)")
+                  return
+              }
+              guard let data = data else {
+                  print("No se recibió data en la respuesta")
+                  return
+              }
+              do {
+                  let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
 
+                  if let userData = json?["data"] as? [String: Any],
+                     let name = userData["name"] as? String,
+                     let email = userData["email"] as? String {
+                      DispatchQueue.main.async {
+                          self.name_txt.text = name
+                          self.email_txt.text = email
+                      }
+                  } else {
+                      print("No se pudo obtener la información del usuario desde la respuesta")
+                  }
+              } catch {
+                  print("Error al convertir la respuesta a JSON: \(error)")
+              }
+          }
+
+          task.resume()
+      }
+    
 }
---
+++ Pods/Target Support Files/Pods-MyLittleAssistant/Pods-MyLittleAssistant-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyLittleAssistant : NSObject
+@end
+@implementation PodsDummy_Pods_MyLittleAssistant
+@end
