a94b8855f6466215b8896079acc82e2e95382a5a
David Herrera <david.gmzherrera28@gmail.com>
2024-03-29 21:55:16 -0600

    se agrego la funcionalidad de editar

--- MyLittleAssistant/EditUserViewController.swift
+++ MyLittleAssistant/EditUserViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 
 
 class EditUserViewController: UIViewController {
-
+    
     @IBOutlet weak var Name_txt: UITextField!
     
     
@@ -21,40 +21,28 @@ class EditUserViewController: UIViewController {
     
     
     
-    @IBOutlet weak var password_txt: UITextField!
-    
-    
-    
-    @IBOutlet weak var Password_Confirmation: UITextField!
-    
-    
-    
-    
-    @IBAction func Confirm_Edit(_ sender: Any) {
+ 
+    @IBAction func Edit_Confirm(_ sender: Any) {
+        UpdateUser()
     }
     
     
     
     
+    @IBOutlet weak var Errors_lbl: UILabel!
+    
+    
+    let userData = UserData.sharedData()
+    var hasErrors = true
+    var user: User = User(id: 0, name: "", email: "")
+    var userName = ""
+    var emailUser = ""
+    var maxLenghts = [UITextField: Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Password_Confirmation.layer.cornerRadius = 10
-        Password_Confirmation.layer.borderWidth = 1
-        Password_Confirmation.layer.borderColor = UIColor.lightGray.cgColor
-        Password_Confirmation.layer.shadowColor = UIColor.gray.cgColor
-        Password_Confirmation.layer.shadowOffset = CGSize(width: 0, height: 2)
-        Password_Confirmation.layer.shadowOpacity = 0.5
-        Password_Confirmation.layer.shadowRadius = 2
         
-        password_txt.layer.cornerRadius = 10
-        password_txt.layer.borderWidth = 1
-        password_txt.layer.borderColor = UIColor.lightGray.cgColor
-        password_txt.layer.shadowColor = UIColor.gray.cgColor
-        password_txt.layer.shadowOffset = CGSize(width: 0, height: 2)
-        password_txt.layer.shadowOpacity = 0.5
-        password_txt.layer.shadowRadius = 2
-                
         Name_txt.layer.cornerRadius = 10
         Name_txt.layer.borderWidth = 1
         Name_txt.layer.borderColor = UIColor.lightGray.cgColor
@@ -62,7 +50,7 @@ class EditUserViewController: UIViewController {
         Name_txt.layer.shadowOffset = CGSize(width: 0, height: 2)
         Name_txt.layer.shadowOpacity = 0.5
         Name_txt.layer.shadowRadius = 2
-                
+        
         Email_txt.layer.cornerRadius = 10
         Email_txt.layer.borderWidth = 1
         Email_txt.layer.borderColor = UIColor.lightGray.cgColor
@@ -73,18 +61,113 @@ class EditUserViewController: UIViewController {
         
         
         let gradientLayer = CAGradientLayer()
-             gradientLayer.frame = view.bounds
-             gradientLayer.colors = [
-                 UIColor(red: 1/255, green: 26/255, blue: 64/255, alpha: 1).cgColor,
-                 UIColor.black.cgColor
-             ]
-                gradientLayer.locations = [0.1, 0.2]
-                gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
-               gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-               
-               view.layer.insertSublayer(gradientLayer, at: 0)
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor(red: 1/255, green: 26/255, blue: 64/255, alpha: 1).cgColor,
+            UIColor.black.cgColor
+        ]
+        gradientLayer.locations = [0.1, 0.2]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
+        
     
+    func UpdateUser() {
+        let url = URL(string: "http://backend.mylittleasistant.online:8000/api/user/update")!
+        var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 50)
+        request.httpMethod = "PUT"
+        
+        let token = userData.jwt
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let name = Name_txt.text!
+        let email = Email_txt.text!
+        
+        let requestBody: [String: Any] = [
+            "name": name,
+            "email": email
+        ]
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+            request.httpBody = jsonData
+        } catch {
+            print("Error al convertir el cuerpo del request a JSON: \(error)")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error en el request: \(error)")
+                self.hasErrors = true
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("No se recibió una respuesta HTTP válida")
+                self.hasErrors = true
+                return
+            }
+            
+            let statusCode = httpResponse.statusCode
+            print("Código de estado HTTP recibido: \(statusCode)")
+            
+            if let data = data {
+                do {
+                    let responseJSON = try JSONSerialization.jsonObject(with: data, options: [])
+                    print("Respuesta JSON: \(responseJSON)")
+                } catch {
+                    print("Error al convertir la respuesta a JSON: \(error)")
+                }
+            } else {
+                print("No se recibió data en la respuesta")
+            }
+            
+            if statusCode == 200 {
+                self.showSuccess(message: "Actualizado correctamente")
+                DispatchQueue.main.async {
+                    if let data = data,
+                       let jsonDict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                       let signedRoute = jsonDict["url"] as? String {
+                        self.hasErrors = false
+                        self.userData.name = name
+                        self.userData.email = email
+                        self.userData.signedRoute = signedRoute
+                    }
+                }
+            } else {
+                print("Error en la solicitud: Código de estado HTTP \(statusCode)")
+                           self.showError(message: "Error en la solicitud: Código de estado HTTP \(statusCode)")
+                           self.hasErrors = true
+            }
+        }
+        
+        task.resume()
+    }
 
+    
+    func showError(message: String) {
+        DispatchQueue.main.async {
+            self.Errors_lbl.isHidden = false
+            self.Errors_lbl.textColor = .red
+            self.Errors_lbl.text = message
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.Errors_lbl.isHidden = true
+            }
+        }
+    }
+    func showSuccess(message: String) {
+        DispatchQueue.main.async {
+            self.Errors_lbl.isHidden = false
+            self.Errors_lbl.textColor = .green
+            self.Errors_lbl.text = message
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.Errors_lbl.isHidden = true
+            }
+        }
+    }
 
 }
